fbd376e90582a1922e8a14b277414e21d9707430
smallwaffles <79935128+smallwaffles@users.noreply.github.com>
2021-03-03 11:08:56 -0700

    Add files via upload

---
+++ ohlp6l3zvre73p5zcazfqchygq-firstParsonsProblems.py
@@ -0,0 +1,38 @@
+"""
+March 2, 2021
+
+Use this document to record your answers to the Parson's Problems
+shared in class.
+
+Remember: the idea behind a Parson's Problem is that you are
+shown completely correct code that is presented OUT OF ORDER.
+When arranged correctly, the code does what the Parson's Problem
+says it should do.
+
+"""
+
+
+#Write your answer to Parson's Problem #1 below:
+for x in range (0, 10):
+    print ("hello world")
+
+
+
+#Write your answer to Parson's Problem #2 below:
+mylist = []
+mylist.append("eggs")
+mylist.append("apple")
+print(mylist)
+
+
+#Write your answer to Parson's Problem #3 below:
+   
+
+    print (" more than nothing")
+x=int(x)
+x=input("thats a number")
+if x > 0:
+     print("thats a small number")
+else:
+    print (" more than nothing")
+    
